212808ebeaab6c287e8aa5ef1cdc896d634e2940
Leonardo G <leoagomes@users.noreply.github.com>
2024-12-05 19:18:49 -0300

    some progress?

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,36 @@
 #include <raylib-cpp.hpp>
+#include <entt/entt.hpp>
+
+class Game {
+public:
+    entt::registry registry;
+};
+
+namespace components {
+    struct Position {
+        Vector3 value;
+    };
+}
 
 int main() {
-    InitWindow(800, 600, "Hello, World!");
+    int screen_width = 800;
+    int screen_height = 600;
+
+    InitWindow(screen_width, screen_height, "Super Collider");
     SetTargetFPS(60);
 
+    Game game;
+    const auto player = game.registry.create();
+    game.registry.emplace<components::Position>(player, Vector3{0, 0, 0});
+
     while (!WindowShouldClose()) {
+        const auto delta_time = GetFrameTime();
+
         BeginDrawing();
         ClearBackground(BLACK);
         EndDrawing();
     }
 
+    CloseWindow();
     return 0;
 }
